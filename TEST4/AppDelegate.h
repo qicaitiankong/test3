2c6608b2de0e066965cfa40e4b626a70d6694e02
qicaitiankong <zihaoshuhan@163.com>
2016-07-29 17:18:03 +0800

    Initial Commit

---
+++ TEST4/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TEST4
+//
+//  Created by 李自豪 on 16/7/29.
+//  Copyright © 2016年 李自豪. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ TEST4/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TEST4
+//
+//  Created by 李自豪 on 16/7/29.
+//  Copyright © 2016年 李自豪. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
