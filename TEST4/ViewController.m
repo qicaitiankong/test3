a63279a2144029a4a49e81b85d29bf00af193622
qicaitiankong <zihaoshuhan@163.com>
2016-08-07 17:01:21 +0800

    789

--- TEST4/ViewController.m
+++ TEST4/ViewController.m
@@ -17,8 +17,6 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     //789
-    self.view.backgroundColor = [UIColor blackColor];
-    self.view.backgroundColor = [UIColor blackColor];
     //插入排序
     NSMutableArray *arr = [[NSMutableArray alloc]initWithObjects:@"45",@"23",@"12",@"33", nil];
     for(NSInteger i = 1; i < arr.count; i ++){
